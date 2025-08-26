a46788b5839d41bbaf6967f6c90003bb9d3c45d2
Vainakh <adlanyandarbiev@gmail.com>
2025-08-26 06:55:43 -0600

    database connection and mongoose.

---
+++ config/database.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+let connected = false;
+
+const connectDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  //if database is already connected, dont connect (hot reload issue);
+
+  if (connected) {
+    console.log('MongoDB is connected!');
+    return;
+  }
+
+  //Connect to MongoDB;
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
