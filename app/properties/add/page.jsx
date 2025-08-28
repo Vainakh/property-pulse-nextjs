b02b20c82bb7eb851fa1f6ec18367e51cec23881
Vainakh <adlanyandarbiev@gmail.com>
2025-08-28 10:19:02 -0600

    Add property form.

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,5 +1,15 @@
+import PropertyAddForm from '@/components/PropertyAddForm';
+
 const AddPropertyPage = () => {
-  return <div>Add Property</div>;
+  return (
+    <section className="bg-gray-100">
+      <div className="container m-auto max-w-xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AddPropertyPage;
