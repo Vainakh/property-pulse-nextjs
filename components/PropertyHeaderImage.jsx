d1c894ffed18b22611b191db922b8cdefaf75435
Vainakh <adlanyandarbiev@gmail.com>
2025-08-30 17:01:10 -0600

    Property card and header image.

--- components/PropertyHeaderImage.jsx
+++ components/PropertyHeaderImage.jsx
@@ -6,8 +6,8 @@ const PropertyHeaderImage = ({ image }) => {
       <div className="container-xl m-auto">
         <div className="grid grid-cols-1">
           <Image
-            src={`/images/properties/${image}`}
-            alt=""
+            src={image}
+            alt="Property Image"
             className="object-cover h-[400px] w-full"
             width={0}
             height={0}
